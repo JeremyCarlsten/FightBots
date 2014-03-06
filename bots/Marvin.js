56e5a708ac97d9eb45c064e4b4c2a0cf6cd0738e
mark oberhaus <moberhau@studentloan.org>
2014-03-06 07:46:56 -0600

    Adding basic clone functionality to marvin

--- bots/Marvin.js
+++ bots/Marvin.js
@@ -1,10 +1,17 @@
-
 var mrvn_targetAquired = 0;
 var mrvn_targetY = 0;
 var mrvn_targetX = 0;
 var mrvn_previousAngle = 0;
 var mrvn_cloneId = 0;
-var mrvn_robotId = 0;
+var mrvn_cloneInit = true;
+
+var isClone = function (robot) {
+    return !!robot.parentId;
+};
+var isRelated = function (robot, scannedRobot) {
+    return robot.id == scannedRobot.parentId || robot.parentId == scannedRobot.id;
+};
+
 
 var calculateAngleToTarget = function (robotX, robotY, absoluteAngle) {
     var deltaX = robotX - mrvn_targetX;
@@ -19,17 +26,19 @@ var calculateAngleToTarget = function (robotX, robotY, absoluteAngle) {
     return angle;
 };
 
-var Robot = function (robot) {};
+var Robot = function (robot) {
+};
 
 Robot.prototype.onIdle = function (ev) {
     var robot = ev.robot;
-  //if(robot.availableClones){
-   //	robot.clone(); 
-   // mrvn_robotId = robot.id();
-    // need to get clone id
-  //}
     var robotX = robot.position.x;
     var robotY = robot.position.y;
+
+    if (robot.availableClones) {
+
+        robot.clone();
+    }
+
     if (mrvn_targetX != 0 && mrvn_targetY != 0) {
         var angle = calculateAngleToTarget(robotX, robotY, robot.cannonAbsoluteAngle);
 
@@ -45,21 +54,28 @@ Robot.prototype.onIdle = function (ev) {
     } else {
         robot.rotateCannon(2);
     }
-    robot.ahead(1);
+    if (isClone(robot)) {
+        if (mrvn_cloneInit) {
+            mrvn_cloneInit = false;
+            robot.turn(90);
+        }
+        robot.ahead(1);
+    } else {
+        robot.ahead(1);
+    }
 };
 
 Robot.prototype.onScannedRobot = function (ev) {
     var robot = ev.robot;
     var target = ev.scannedRobot;
-  
-  //if(target.id != mrvn_robotId){ 
 
-    mrvn_targetAquired = 50;
+    if (!isRelated(robot, target)) {
+        mrvn_targetAquired = 50;
 
-    mrvn_targetX = target.position.x;
-    mrvn_targetY = target.position.y;  
-    robot.fire();
-  //}
+        mrvn_targetX = target.position.x;
+        mrvn_targetY = target.position.y;
+        robot.fire();
+    }
 };
 
 Robot.prototype.onWallCollision = function (ev) {
@@ -68,13 +84,13 @@ Robot.prototype.onWallCollision = function (ev) {
     robot.turn(ev.bearing + 90);
 };
 
-Robot.prototype.onRobotCollision = function (ev) { 
+Robot.prototype.onRobotCollision = function (ev) {
     var robot = ev.robot;
-    robot.back(20); 
+    robot.back(20);
 };
 
-Robot.prototype.onHitByBullet = function (ev) {     
-    var robot = ev.robot;      
+Robot.prototype.onHitByBullet = function (ev) {
+    var robot = ev.robot;
     //robot.rotateCannon(ev.bearing);
     if (robot.availableDisappears > 0) {
         robot.disappear();
